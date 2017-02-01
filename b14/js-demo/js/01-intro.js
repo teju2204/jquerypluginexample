3a237e1c60c071ddce1737b428778c5e16fb155c
teju2204 <thejaswinics22@gmail.com>
2017-02-01 21:26:31 +0530

    my first commit

---
+++ b14/js-demo/js/01-intro.js
@@ -0,0 +1,15 @@
+  var elem = document.getElementById('creative');
+  console.log(elem.textContent);
+  console.log(elem.innerHTML);
+
+/*
+  elem.style.color = "red";
+  elem.style.textTransform = "uppercase";
+  elem.style.backgroundColor = "green";
+*/
+  elem.className = "aa"; //single line comment
+var clsname = elem.className;
+console.log(clsname);
+console.log(elem.style.color);
+  
+
